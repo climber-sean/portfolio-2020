7560bd184dc983ff638cb5ea5f764c369326ef08
Sean Butlin <seanbutlin@googlemail.com>
2020-06-09 11:35:39 +0100

    changes to nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,11 +5,11 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: 'Sean Butlin Frontend Developer',
+    title: 'Sean Butlin | Frontend Developer',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: 'Sean Butlin | Frontend Web Developer Staffordshire' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
